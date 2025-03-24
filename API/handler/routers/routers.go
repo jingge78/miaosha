8458bfe56b16238e91b698058479b2d42f06c16e
居吉良 <1772528362@qq.com>
2025-03-24 11:07:33 +0800

    0324

--- API/handler/routers/routers.go
+++ API/handler/routers/routers.go
@@ -1,1 +1,17 @@
 package routers
+
+import "github.com/gin-gonic/gin"
+
+func LoadRouters(r *gin.Engine) {
+	user := r.Group("/user")
+	{
+		user.POST("/login", UserLogin)
+		user.POST("/register", UserRegister)
+	}
+	product := r.Group("/product")
+	{
+		product.GET("/detail", ProductDetail)
+		product.GET("/list", ProductList)
+		product.POST("/sync/es", ProductSyncEs)
+	}
+}
