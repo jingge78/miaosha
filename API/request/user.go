fdff2586809e8ead721607690b0e97d260bbd94d
高奇睿2 <3539016903@qq.com>
2025-03-25 20:28:53 +0800

    gqr

--- API/request/user.go
+++ API/request/user.go
@@ -12,3 +12,13 @@ type Update struct {
 	Account  string `form:"account" binding:"required"`
 	Password string `form:"password" binding:"required"`
 }
+
+type SendSms struct {
+	Mobile string `form:"mobile" binding:"required"`
+}
+
+type PassWordRecovery struct {
+	Account string `form:"account" binding:"required"`
+	Mobile  string `form:"mobile" binding:"required"`
+	SendSms string `form:"sendSms" binding:"required"`
+}
--- service/user_service/user_login.go
+++ service/user_service/user_login.go
@@ -23,7 +23,7 @@ func (s *ServerUser) Login(ctx context.Context, in *user.LoginRequest) (*user.Lo
 	if users.Uid == 0 {
 		return nil, errors.New("用户不存在，请前往注册")
 	}
-	if users.Pwd != utils.Md5(in.Password) {
+	if users.Pwd != utils.Encryption(in.Password) {
 		return nil, errors.New("密码错误")
 	}
 	return &user.LoginResponse{UserId: int64(users.Uid)}, err
